5bff419c739c2db3f058303395809243703318cb
Blair Bonnett <blair.bonnett@gmail.com>
2019-01-07 15:10:00 +1300

    Update copyright year.

--- pgfutils.py
+++ pgfutils.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Blair Bonnett
+# Copyright 2018, 2019 Blair Bonnett
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
