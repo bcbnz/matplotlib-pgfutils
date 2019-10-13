1c4846a2235d19e5f48ec71473effa14194677ad
Blair Bonnett <blair.bonnett@gmail.com>
2019-10-13 11:04:53 +0200

    Tests: use pytest.skip instead of silently ignoring tests.

--- tests/test_external.py
+++ tests/test_external.py
@@ -1,33 +1,30 @@
 import os.path
 from .utils import build_figure, clean_dir
+import pytest
 
-base = os.path.normpath(os.path.dirname(__file__))
-
-try:
-    import cartopy
-except ImportError:
-    cartopy = None
-
-try:
-    import seaborn
-except ImportError:
-    seaborn = None
+srcdir = os.path.join(os.path.normpath(os.path.dirname(__file__)), "sources", "external")
 
 
-if cartopy:
-    def test_cartopy():
-        """Check a cartopy figure can be generated..."""
-        dir = os.path.join(base, "sources", "external")
-        res = build_figure(dir, "cartopy_figure.py")
-        assert res.returncode == 0, "tests/sources/external/cartopy_figure.py could not be built."
-        clean_dir(dir)
+def test_cartopy():
+    """Check a cartopy figure can be generated..."""
+    try:
+        import cartopy
+    except ImportError:
+        pytest.skip("cartopy not available for testing")
+
+    res = build_figure(srcdir, "cartopy_figure.py")
+    assert res.returncode == 0, "tests/sources/external/cartopy_figure.py could not be built."
+    clean_dir(srcdir)
 
 
-if seaborn:
-    def test_seaborn():
-        """Check a seaborn figure can be generated..."""
-        dir = os.path.join(base, "sources", "external")
-        res = build_figure(dir, "seaborn_figure.py")
-        assert res.returncode == 0, "tests/sources/external/seaborn_figure.py could not be built."
-        clean_dir(dir)
+def test_seaborn():
+    """Check a seaborn figure can be generated..."""
+    try:
+        import seaborn
+    except ImportError:
+        pytest.skip("seaborn not available for testing")
+
+    res = build_figure(srcdir, "seaborn_figure.py")
+    assert res.returncode == 0, "tests/sources/external/seaborn_figure.py could not be built."
+    clean_dir(srcdir)
 
